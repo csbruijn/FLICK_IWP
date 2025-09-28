284387c8eff42f6976faf875c67eb0ce050f20f7
csbruijn <corsnee@gmail.com>
2025-09-28 16:41:10 +0200

    Extra Drum Version

--- Assets/_Game/AudioObjects/Scripts/DrumsController.cs
+++ Assets/_Game/AudioObjects/Scripts/DrumsController.cs
@@ -1,12 +1,43 @@
+using FMODUnity;
+using Unity.VisualScripting;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class DrumsController : MonoBehaviour
 {
-    public GameEvent OnDrumsPlayed;
+    
+    private StudioEventEmitter studioEventEmitter; 
 
-    public void DrumsButtonPressed()
+    private Animator[] StickAnimators;
+    
+    void Awake()
     {
-        OnDrumsPlayed.Raise(this, null);
-        Debug.Log("Play the drums");
+        studioEventEmitter = GetComponent<StudioEventEmitter>();
+        StickAnimators = GetComponentsInChildren<Animator>();
+        Debug.Log(StickAnimators.Length);
     }
+
+    public void OnDrumPlayed()
+    {
+        Animator Stick = StickAnimators[0];
+
+
+        if (Stick.GetCurrentAnimatorStateInfo(0).IsName("Resting"))
+        {
+            Stick.SetTrigger("Drum");
+            studioEventEmitter.Play();
+        }
+        else
+        {
+            Stick = StickAnimators[1];
+
+            if (Stick.GetCurrentAnimatorStateInfo(0).IsName("Resting"))
+            {
+                Stick.SetTrigger("Drum");
+
+                studioEventEmitter.Play();
+            }
+        }
+    }
+    
 }
---
+++ Assets/_Game/AudioObjects/Scripts/DrumsView.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DrumsView : MonoBehaviour
+{
+    public GameEvent OnDrumsPlayed;
+
+    public void DrumsButtonPressed()
+    {
+        OnDrumsPlayed.Raise(this, null);
+        Debug.Log("Play the drums");
+    }
+}
