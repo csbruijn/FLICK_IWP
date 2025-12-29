611c0320d652d556898978598fe75ad8c4ac49bb
kid-of-yesterday <kurlusha2@yandex.ru>
2025-12-29 23:33:11 +0300

    siren attacks now fire at intervals
    you can change how often those activate in wave spawner - attack behaviour. also accidentally added countdown timer (doesn't trigger game over)

--- Assets/_Game/Level/Scripts/CountDownView.cs
+++ Assets/_Game/Level/Scripts/CountDownView.cs
@@ -3,10 +3,36 @@ using UnityEngine;
 
 public class CountDownView : MonoBehaviour
 {
+
+    //i handle the countdown updates here. updates every frame for now
+
+    [SerializeField] private GameEvent onCountDownChanged;
+    [SerializeField] private float startTime = 10f;
+
+    private float currentTime;
+
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    void Update()
+    {
+        if (currentTime <= 0f) return;
+
+        currentTime -= Time.deltaTime;
+        onCountDownChanged.Raise(this, currentTime);
+    }
+
+    //the time is in seconds now
     public void OnCountDownChanged(Component sender, System.Object data)
     {
         float timeToDisplay = (float)data;
 
-        GetComponent<TextMeshProUGUI>().text = timeToDisplay.ToString();
+        GetComponent<TextMeshProUGUI>().text = Mathf.CeilToInt(timeToDisplay).ToString();
+
+        //Debug.Log("countdown script is working");
     }
+
+
 }
--- Assets/_Game/Level/Scripts/Gamemanager.cs
+++ Assets/_Game/Level/Scripts/Gamemanager.cs
@@ -18,6 +18,7 @@ public class Gamemanager : MonoBehaviour
 
    
     [SerializeField] private GameEvent OnGameOVer;
+    [SerializeField] private GameEvent OnGameStarted;
 
     bool outcomeSet = false;
 
@@ -32,6 +33,8 @@ public class Gamemanager : MonoBehaviour
     private void Start()
     {
         GameStarted = true;
+        OnGameStarted.Raise(this, null);
+        Debug.Log("game start");
 
     }
 
