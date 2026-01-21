b459bd82782d51a83e2e9025abd60537edc61e01
csbruijn <corsnee@gmail.com>
2026-01-21 23:26:27 +0100

    fixes

--- Assets/_Game/2D Assets/Player/GhostPlatformController.cs
+++ Assets/_Game/2D Assets/Player/GhostPlatformController.cs
@@ -2,58 +2,58 @@ using UnityEngine;
 
 public class GhostPlatformController : MonoBehaviour
 {
-    [SerializeField] private GameObject ghostPlatform;
-    [SerializeField] private float activationDistance = 1.3f;
+    //[SerializeField] private GameObject ghostPlatform;
+    //[SerializeField] private float activationDistance = 1.3f;
 
-    private PlayerStatus selfStatus;
-    private PlayerController selfController;
+    //private PlayerStatus selfStatus;
+    //private PlayerController selfController;
 
-    void Awake()
-    {
-        selfStatus = GetComponent<PlayerStatus>();
-        selfController = GetComponent<PlayerController>();
-
-
-        Debug.Log("GHOST PLATFORM AWAKE", this);
+    //void Awake()
+    //{
+    //    selfStatus = GetComponent<PlayerStatus>();
+    //    selfController = GetComponent<PlayerController>();
 
 
-    }
+    //    Debug.Log("GHOST PLATFORM AWAKE", this);
 
-    void Update()
-    {
+
+    //}
+
+    //void Update()
+    //{
         
 
-        if (!selfStatus.isDead)
-        {
-            ghostPlatform.SetActive(false);
-            return;
-        }
+    //    if (!selfStatus.isDead)
+    //    {
+    //        ghostPlatform.SetActive(false);
+    //        return;
+    //    }
 
-        Debug.Log("I AM DEAD (GHOST)");
+    //    Debug.Log("I AM DEAD (GHOST)");
 
-        int otherIndex = 1 - selfController.playerIndex;
+    //    int otherIndex = 1 - selfController.playerIndex;
 
-        PlayerController other =
-            Gamemanager.instance.playerControllers[otherIndex];
+    //    PlayerController other =
+    //        Gamemanager.instance.playerControllers[otherIndex];
 
-        if (other == null)
-        {
-            ghostPlatform.SetActive(false);
-            return;
-        }
-
-
-        if (other.playerStatus.isDead)
-        {
-            Debug.Log("EXIT: other is dead");
-            ghostPlatform.SetActive(false);
-            return;
-        }
+    //    if (other == null)
+    //    {
+    //        ghostPlatform.SetActive(false);
+    //        return;
+    //    }
 
 
-        float dist = Vector2.Distance(transform.position, other.transform.position);
-        Debug.Log("Distance to other for platform: " + dist);
+    //    if (other.playerStatus.isDead)
+    //    {
+    //        Debug.Log("EXIT: other is dead");
+    //        ghostPlatform.SetActive(false);
+    //        return;
+    //    }
 
-        ghostPlatform.SetActive(dist < activationDistance);
-    }
+
+    //    float dist = Vector2.Distance(transform.position, other.transform.position);
+    //    Debug.Log("Distance to other for platform: " + dist);
+
+    //    ghostPlatform.SetActive(dist < activationDistance);
+    //}
 }
